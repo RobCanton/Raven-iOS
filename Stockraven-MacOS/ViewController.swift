aa6d095ac338d4e2740a19277897dd8bac29c682
Robert Canton <robshanecanton@gmail.com>
2020-05-13 08:48:14 -0400

    Fixed stock cell update issue: moveStock was not updating index.

---
+++ Stockraven-MacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stockraven
+//
+//  Created by Robert Canton on 2020-05-02.
+//  Copyright © 2020 Robert Canton. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
